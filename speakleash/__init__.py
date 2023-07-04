8d3d7e8ba6d89cb750e2c846b1e14b3d9af63757
jchwila <jacek.chwila@gmail.com>
2023-07-04 22:29:16 +0200

    Update __init__.py

--- speakleash/__init__.py
+++ speakleash/__init__.py
@@ -1,17 +1,19 @@
 import requests
 import json
+from tqdm import tqdm
 import os
+from lm_dataformat import Reader
 import hashlib
+from datetime import datetime
 import glob
 import tempfile
-from tqdm import tqdm
-from lm_dataformat import Reader
-from datetime import datetime
 
 class FileManager:
+
     @staticmethod
     def ensure_dir_exists(directory):
-        os.makedirs(directory, exist_ok=True)
+        if not os.path.exists(directory):
+            os.makedirs(directory, exist_ok=True)
 
     @staticmethod
     def load_json(file):
@@ -51,12 +53,11 @@ class FileManager:
             return False
 
 class WebRequester:
-    session = requests.Session()
 
     @staticmethod
     def get_json(url):
         try:
-            r = WebRequester.session.get(url)
+            r = requests.get(url)
             if r.ok:
                 return json.loads(r.text)
         except:
@@ -65,7 +66,7 @@ class WebRequester:
     @staticmethod
     def get_text(url, encoding='utf-8'):
         try:
-            r = WebRequester.session.get(url)
+            r = requests.get(url)
             r.encoding = encoding
             if r.ok:
                 return r.text
@@ -74,34 +75,20 @@ class WebRequester:
 
     @staticmethod
     def download_file(url, filepath):
-        try:
-            response = requests.get(url, stream=True)
-            response.raise_for_status()  # Raise an exception for HTTP errors
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred while making the request: {e}")
-            return False
-    
-        try:
-            total_size_in_bytes = int(response.headers.get('content-length', 0))
-        except (ValueError, TypeError):
-            print("An error occurred while calculating the file size")
-            return False
-    
-        try:
-            block_size = 1024
-            progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-            with open(filepath, 'wb') as file:
-                for data in response.iter_content(block_size):
-                    progress_bar.update(len(data))
-                    file.write(data)
-            progress_bar.close()
-        except IOError as e:
-            print(f"An error occurred while writing the file: {e}")
-            return False
-
+        response = requests.get(url, stream=True)
+        total_size_in_bytes = int(response.headers.get('content-length', 0))
+        block_size = 1024
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+        with open(filepath, 'wb') as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
         return total_size_in_bytes == progress_bar.n
 
-class StructureDownloader:
+
+class StructureDownloader(object):
+
     def __init__(self, replicate_dir):
         self.replicate_dir = replicate_dir
 
@@ -129,7 +116,8 @@ class StructureDownloader:
         FileManager.save_json(data, file)
         return data
 
-class CategoryManager:
+class CategoryManager(object):
+
     def __init__(self):
         self.temp_dir = os.path.join(tempfile.gettempdir(), "speakleash")
         FileManager.ensure_dir_exists(self.temp_dir)
@@ -171,7 +159,8 @@ class CategoryManager:
                     return True
         return False
 
-class Speakleash:
+class Speakleash(object):
+
     def __init__(self, replicate_dir, lang="pl"):
         self.replicate_dir = replicate_dir
         self.datasets = []
@@ -186,7 +175,7 @@ class Speakleash:
 
         names = self.structure_downloader.get_structure(url + structure_file)
 
-        if names:
+        if names:      
             for item in names:
                 if "name" in item:
                     self.datasets.append(SpeakleashDataset(item["name"], url, self.replicate_dir))
@@ -194,11 +183,141 @@ class Speakleash:
     def get(self, name):
         return next((d for d in self.datasets if d.name == name), None)
 
-class SpeakleashDataset:
+class SpeakleashDataset(object):
+
     def __init__(self, name, url, replicate_dir):
         self.url = url
         self.name = name
         self.replicate_dir = replicate_dir
         self.structure_downloader = StructureDownloader(self.replicate_dir)
         self.manifest = self._download_manifest()
-        self.jsonl_zst
+
+    def _download_manifest(self):
+        data = self.structure_downloader.get_structure(self.url + self.name + ".manifest")
+        if data:
+            return data
+        else:
+            print(f"Error downloading manifest {self.url + self.name + '.manifest'}")
+        return {}
+
+    def _download_file(self, file_name):
+        url = self.url + self.name + ".jsonl.zst"
+        file_path = os.path.join(self.replicate_dir, file_name)
+        return WebRequester.download_file(url, file_path)
+
+    def check_file(self):
+        FileManager.ensure_dir_exists(self.replicate_dir)
+
+        file_name_json_zst = os.path.join(self.name + ".jsonl.zst")
+        file_path_json_zst = os.path.join(self.replicate_dir, file_name_json_zst)
+
+        file_json_zst_exists = os.path.exists(file_path_json_zst) and os.path.getsize(file_path_json_zst) == self.jsonl_zst_file_size
+
+        if not file_json_zst_exists and not self._download_file(file_name_json_zst):
+            return False, ""
+
+        return True, file_path_json_zst
+
+    def _get_data(self, get_meta=False):
+        ok, file_path_json_zst = self.check_file()
+        if not ok:
+            return None
+
+        rdr = Reader(file_path_json_zst)
+        return rdr.stream_data(get_meta=get_meta)
+
+    def _get_stat(self, *args):
+        stat = self.manifest.get('stats', {})
+        for arg in args:
+            stat = stat.get(arg, {})
+        return stat
+
+    @property
+    def data(self):
+        return self._get_data()
+
+    @property
+    def ext_data(self):
+        return self._get_data(get_meta=True)
+
+    @property
+    def samples(self):
+        return self.structure_downloader.get_structure(self.url + self.name + ".sample", False) or []
+
+    @property
+    def description(self):
+        return self.manifest.get('description', '')
+
+    @property
+    def license(self):
+        return self.manifest.get('license', '')
+
+    @property
+    def category(self):
+        return self.manifest.get('category', '')
+
+    @property
+    def sources(self):
+        return self.manifest.get('sources', {})
+
+    @property
+    def characters(self):
+        return self._get_stat('characters')
+
+    @property
+    def quality_metrics(self):
+        return any(self._get_stat('quality', q) != 0 for q in ['HIGH', 'LOW', 'MEDIUM'])
+
+    @property
+    def categorization(self):
+        return any(val > 0 for val in self.manifest.get('category=95%', {}).values())
+
+    @property
+    def categories(self):
+        return self.manifest.get('category=95%', {})
+
+    @property
+    def quality(self):
+        return self._get_stat('quality')
+
+    @property
+    def documents(self):
+        return self._get_stat('documents')
+
+    @property
+    def stopwords(self):
+        return self._get_stat('stopwords')
+
+    @property
+    def jsonl_zst_file_size(self):
+        return self.manifest.get('file_size', 0)
+
+    @property
+    def nouns(self):
+        return self._get_stat('nouns')
+
+    @property
+    def verbs(self):
+        return self._get_stat('verbs')
+
+    @property
+    def symbols(self):
+        return self._get_stat('symbols')
+
+    @property
+    def punctuations(self):
+        return self._get_stat('punctuations')
+
+    @property
+    def sentences(self):
+        return self._get_stat('sentences')
+
+    @property
+    def words(self):
+        return self._get_stat('words')
+
+    def __repr__(self):
+        return f"SpeakleashDataset([{self.name},{self.url},{self.characters}])"
+
+    def __str__(self):
+        return f"name: {self.name}, url: {self.url}, characters: {self.characters}"
