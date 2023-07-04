a5b578503201a6065d469dc7c46e3adc8d59a2c5
jchwila <jacek.chwila@gmail.com>
2023-07-04 22:09:44 +0200

    Update __init__.py
    Added download exception handling

--- speakleash/__init__.py
+++ speakleash/__init__.py
@@ -75,7 +75,9 @@ class WebRequester:
 
     @staticmethod
     def download_file(url, filepath):
+    try:
         response = requests.get(url, stream=True)
+        response.raise_for_status()  # Raise an exception for non-successful status codes
         total_size_in_bytes = int(response.headers.get('content-length', 0))
         block_size = 1024
         progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
@@ -85,6 +87,12 @@ class WebRequester:
                 file.write(data)
         progress_bar.close()
         return total_size_in_bytes == progress_bar.n
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading file: {e}")
+        return False
+    except IOError as e:
+        print(f"Error writing file: {e}")
+        return False
 
 
 class StructureDownloader(object):
